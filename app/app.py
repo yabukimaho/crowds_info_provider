f17b661379549df59c5a662316337ab1f8d24164
saitotm <tomokazu.ml.tech@gmail.com>
2019-12-11 20:21:32 +0900

    混雑状況デモ機能追加

--- app/app.py
+++ app/app.py
@@ -2,8 +2,19 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='.', static_url_path='')
 
+THRESHOLD = 1500
+
 @app.route('/')
 def index():
-    return app.send_static_file('templates/index.html')
+    if get_co2() > THRESHOLD:
+        message = '混雑しています'
+    else:
+        message = '空いています'
+
+    return render_template('index.html', state=message)
+
+def get_co2():
+    return 1000
+
 
 app.run(port=8000, debug=True)
